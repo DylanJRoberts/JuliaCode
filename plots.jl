049111e90936602901a824903973e2ffb4d9a3d1
DRoberts <64997368+DylanJRoberts@users.noreply.github.com>
2024-01-12 20:18:07 +0000

    added plots.jl

---
+++ plots.jl
@@ -0,0 +1,12 @@
+# Import the Plots library
+using Plots
+
+# Create some sample data
+x = 1:10
+y = rand(10)
+
+# Create a plot
+plot(x, y, label="Random Data", xlabel="X-axis", ylabel="Y-axis", title="Sample Plot")
+
+# Show the plot
+display(plt)
